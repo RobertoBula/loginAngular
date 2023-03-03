53475331e03a40de9104a4c70941f08fa9d56f5b
RobertoBula <mozrobertocarlos@gmail.com>
2023-03-03 10:01:26 -0500

    add config deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAZIg5voNqeSyYeMNOhaE-clammY9Lo4ms",
+    authDomain: "login-firebase-cd617.firebaseapp.com",
+    projectId: "login-firebase-cd617",
+    storageBucket: "login-firebase-cd617.appspot.com",
+    messagingSenderId: "258585922502",
+    appId: "1:258585922502:web:9fc2eba4b85d7593dec0c7"
+  }
 };
